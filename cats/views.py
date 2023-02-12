2f41c06d524fdad9cb8f5e0d48a2569ec19cdcd4
Николай Белоусько <belnv@yandex.ru>
2023-02-12 17:55:44 +0300

    Перешел на другой комп

--- cats/views.py
+++ cats/views.py
@@ -1,9 +1,19 @@
 from rest_framework import viewsets
 
-from .models import Cat
-from .serializers import CatSerializer
+from .models import Cat, Owner, Achievement
+from .serializers import CatSerializer, OwnerSerializer, AchievementSerializer
 
 
 class CatViewSet(viewsets.ModelViewSet):
     queryset = Cat.objects.all()
     serializer_class = CatSerializer
+
+
+class OwnerViewSet(viewsets.ModelViewSet):
+    queryset = Owner.objects.all()
+    serializer_class = OwnerSerializer
+
+
+class AchievementViewSet(viewsets.ModelViewSet):
+    queryset = Achievement.objects.all()
+    serializer_class = AchievementSerializer
--- kittygram_plus/urls.py
+++ kittygram_plus/urls.py
@@ -2,11 +2,13 @@ from django.urls import include, path
 
 from rest_framework.routers import DefaultRouter
 
-from cats.views import CatViewSet
+from cats.views import CatViewSet, OwnerViewSet, AchievementViewSet
 
 
 router = DefaultRouter()
 router.register('cats', CatViewSet)
+router.register('owners', OwnerViewSet)
+router.register('achievements', AchievementViewSet)
 
 urlpatterns = [
     path('', include(router.urls)),
