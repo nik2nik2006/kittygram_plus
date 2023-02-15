b98f7b24feec7aeabd20406fc833fc349a771ceb
никола белоусько <belnv@yandex.ru>
2023-02-15 18:05:52 +0300

    Закончил теорию 8 спринта

--- kittygram_plus/urls.py
+++ kittygram_plus/urls.py
@@ -2,14 +2,20 @@ from django.urls import include, path
 
 from rest_framework.routers import DefaultRouter
 
-from cats.views import CatViewSet, OwnerViewSet, AchievementViewSet
+from cats.views import CatViewSet, OwnerViewSet, AchievementViewSet, LightCatViewSet
 
 
 router = DefaultRouter()
 router.register('cats', CatViewSet)
 router.register('owners', OwnerViewSet)
 router.register('achievements', AchievementViewSet)
+router.register(r'mycats', LightCatViewSet)
 
 urlpatterns = [
     path('', include(router.urls)),
+    # Djoser создаст набор необходимых эндпоинтов.
+    # базовые, для управления пользователями в Django:
+    path('auth/', include('djoser.urls')),
+    # JWT-эндпоинты, для управления JWT-токенами:
+    path('auth/', include('djoser.urls.jwt')),
 ]
